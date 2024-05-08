e50e52b78f40c52a666c2a5eed9356bb27c9934c
phantomrebirth <ahmedxotwod2@gmail.com>
2024-05-08 08:33:23 +0300

    added t videos

--- src/components/courses/Videos.jsx
+++ src/components/courses/Videos.jsx
@@ -1,45 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import { Container, Row } from 'react-bootstrap';
+import React, { useEffect, useRef, useState } from 'react'
+import { Alert, Card, Col, Container, Row } from 'react-bootstrap';
 import ReactPlayer from 'react-player';
-import { selectRole } from '../../redux/slices/authSlice';
+import { selectRole, selectToken } from '../../redux/slices/authSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCourses, selectCourses } from '../../redux/slices/coursesSlice';
 import LoadingSpinner from '../../redux/actions/LoadingSpinner';
 import { HiOutlineDownload } from "react-icons/hi";
+import axios from 'axios';
+import { IoAdd } from 'react-icons/io5';
+import { MdOutlineDeleteOutline } from "react-icons/md";
 
 const Videos = () => {
-
     const dispatch = useDispatch();
-
     const role = useSelector(selectRole);
+    const token = useSelector(selectToken);
     const [teacher, setTeacher] = useState(false);
     const [student, setStudent] = useState(false);
+    const [uploadAlert, setUploadAlert] = useState(null);
+    const [deleteAlert, setDeleteAlert] = useState(null);
     useEffect(() => {
         if (role === 'student') {
-          setStudent(true);
+            setStudent(true);
         } else if (role === 'teacher') {
-          setTeacher(true);
+            setTeacher(true);
         }
         dispatch(fetchCourses());
     }, [role, dispatch]);
     const { loading, data: courses, currentCourseId } = useSelector(selectCourses);
-  
     const course = courses.find(course => course._id === currentCourseId);
     const videoPath = course.videos.map(video => `https://ezlearn.onrender.com/course/getVideos/${currentCourseId}/${video._id}`);
     const videosPaths = course.videos.map(video => {
         const matchingPath = videoPath.find(path => path.includes(video._id));
-      return {
-          ...video,
-          path: matchingPath || ''
-      };
+        return {
+            ...video,
+            path: matchingPath || ''
+        };
     });
+    const fileInputRef = useRef(null);
 
     const handleVideoDownload = async (video) => {
       const { filename, path } = video;
       try {
           const response = await fetch(path);
           const fileData = await response.arrayBuffer();
-
           const a = document.createElement('a');
           a.href = URL.createObjectURL(new Blob([fileData]));
           a.download = filename;
@@ -51,10 +54,63 @@ const Videos = () => {
       };
     };
 
+    const handleVideoUpload = async (event) => {
+        const file = event.target.files[0];
+        const formData = new FormData();
+        formData.append('videos', file);
+    
+        try {
+            const response = await axios.post(`https://ezlearn.onrender.com/course/videos/${currentCourseId}`, formData, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            if (response.status === 200 || response.status === 201) {
+                console.log('Video uploaded successfully!');
+                setUploadAlert({ variant: 'primary', message: 'Video uploaded successfully!' });
+                dispatch(fetchCourses());
+            } else {
+                console.error('Failed to upload video:', response.statusText);
+                setUploadAlert({ variant: 'danger', message: `Failed to upload video: ${response.statusText}` });
+            }
+        } catch (error) {
+            console.error('Error uploading video:', error);
+            setUploadAlert({ variant: 'danger', message: `Error uploading video: ${error.message}` });
+        }
+    };
+
+    const handleVideoDelete = async (video) => {
+    
+        try {
+            const response = await axios.delete(`https://ezlearn.onrender.com/course/deleteVideos/${currentCourseId}/${video._id}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            if (response.status === 200 || response.status === 201) {
+                console.log('Video deleted successfully!');
+                setDeleteAlert({ variant: 'primary', message: 'Video deleted successfully!' });
+                dispatch(fetchCourses());
+            } else {
+                console.error('Failed to delete video:', response.statusText);
+                setDeleteAlert({ variant: 'danger', message: `Failed to delete video: ${response.statusText}` });
+            }
+        } catch (error) {
+            console.error('Error deleting video:', error);
+            setDeleteAlert({ variant: 'danger', message: `Error deleting video: ${error.message}` });
+        }
+    };
+
+    const handleButtonClick = () => {
+        fileInputRef.current.click();
+    };
+
+    const thumbnailUrl = "rgb(240, 240, 240)"
 //   const videoUrl = "https://www.youtube.com/watch?v=fkZK4MqqNTY"
     // const videoUrl = require('/home/phantom/Documents/react-project/src/assets/test/vid0.mp4');
     // const thumbnailUrl = "https://www.rollingstone.com/wp-content/uploads/2020/02/THE-WEEKND-by-Duncan-Loudon.jpg?w=1600&h=900&crop=1"
-    const thumbnailUrl = "rgb(240, 240, 240)"
   // const [videoUrl, setVideoUrl] = useState('');
 
   // useEffect(() => {
@@ -82,12 +138,14 @@ const Videos = () => {
 
   return (
     <>
+        {uploadAlert && <Alert variant={uploadAlert.variant} onClose={() => setUploadAlert(false)} dismissible>{uploadAlert.message}</Alert>}
+        {deleteAlert && <Alert variant={deleteAlert.variant} onClose={() => setDeleteAlert(false)} dismissible>{deleteAlert.message}</Alert>}
         {student && (
-            <Container className='videos-container' style={{ padding: "0" }}>
-                {videosPaths.map((video, index) => {
+            <Container className='videos-container' style={{ padding: "0", margin: "1rem 0 0 0", flexWrap: "wrap", maxWidth: "none" }}>
+                {videosPaths.slice().reverse().map((video, index) => {
                     const fileNameWithoutExtension = video.filename.split('.').slice(0, -1).join('.');                        
                     return (
-                        <Row key={index} className='players-container' style={{ padding: "0", margin: "0" }}>
+                        <Row key={index} className='players-container' style={{ padding: "0", margin: "0", width: "auto" }}>
                             <div className='player-container'>
                                 <div className='video-player'>
                                     <ReactPlayer className='video'
@@ -113,7 +171,63 @@ const Videos = () => {
             </Container>
         )}
         {teacher && (
-            <div>_</div>
+          <Container className='videos-container' style={{ padding: "0", margin: "1rem 0 0 0", flexWrap: "wrap", maxWidth: "none" }}>
+            <Row className='players-container' style={{ padding: "0", margin: "0", display: "block", width: "auto" }}>
+                <div style={{justifyContent: "center", width: "100%"}}>
+                <div style={{ paddingBottom: "10%"}}>
+                    <input 
+                        ref={fileInputRef} 
+                        type="file" 
+                        accept="video/*" 
+                        onChange={handleVideoUpload} 
+                        style={{ display: 'none' }}
+                    />
+                    <Col className='reminder-card' style={{ padding: "0", margin: "0" }}>
+                        <Card style={{ padding: "0", width: "25rem", height: "16.6rem", margin: "0", borderRadius: "1.25rem" }}
+                              className='addR-container'
+                              onClick={handleButtonClick}
+                              title='Upload video'
+                        >
+                        <Card.Body>
+                            <Card.Title className='addReminder-body'>
+                            <IoAdd className='addReminder-icon'/>
+                            </Card.Title>
+                        </Card.Body>
+                        </Card>
+                    </Col>
+                </div>
+                </div>
+            </Row>
+            {videosPaths.slice().reverse().map((video, index) => {
+                const fileNameWithoutExtension = video.filename.split('.').slice(0, -1).join('.');                        
+                return (
+                    <Row key={index} className='players-container' style={{ padding: "0", margin: "0", width: "auto" }}>
+                        <div className='player-container'>
+                            <div className='video-player'>
+                                <ReactPlayer className='video'
+                                    url={video.path}
+                                    controls
+                                    type="video/mp4"
+                                    light={thumbnailUrl}>
+                                </ReactPlayer>
+                            </div>
+                            <div style={{display: "flex", alignItems: "center", justifyContent: "center", margin: "0.3rem 0% 0.5rem 0% "}}>
+                                <p className='video-title'>
+                                    {/* {`Section ${index + 1}`} */}
+                                    {fileNameWithoutExtension}
+                                </p>
+                                <a onClick={() => handleVideoDownload(video)}>
+                                    <HiOutlineDownload className='downloadVideo-icon' title='Download'/>
+                                </a>
+                                <a onClick={() => handleVideoDelete(video)}>
+                                    <MdOutlineDeleteOutline className='deleteVideo-icon' title='Delete'/>
+                                </a>
+                            </div>
+                        </div>
+                    </Row>
+                );
+            })}
+          </Container>        
         )}
     </>
   );
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -74,23 +74,33 @@
 
 // export default Courses;
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCourses, selectCourses } from '../redux/slices/coursesSlice';
 import { Container, Card, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaChevronRight } from "react-icons/fa6";
 import LoadingSpinner from '../redux/actions/LoadingSpinner';
+import { selectRole } from '../redux/slices/authSlice';
 
 function Courses() {
   const dispatch = useDispatch();
+  const role = useSelector(selectRole);
+  const [teacher, setTeacher] = useState(false);
+  const [student, setStudent] = useState(false);
+  console.log(role);
+  useEffect(() => {
+      if (role === 'student') {
+        setStudent(true);
+      } else if (role === 'teacher') {
+        setTeacher(true);
+      }
+      dispatch(fetchCourses());
+  }, [role, dispatch]);
   const { data: courses, loading, error } = useSelector(selectCourses);
 
-  useEffect(() => {
-    dispatch(fetchCourses());
-  }, [dispatch]);
 
-  // console.log('Courses:', courses);
+  console.log('Courses:', courses);
 
   return (
     <>
@@ -99,31 +109,64 @@ function Courses() {
       ) : error ? (
         <p>Error: {error}</p>
       ) : (
-        <Container className='cards-container' style={{ padding: "0px" }} fluid>
-          <Row xs={1} md={2} className="g-4" style={{ padding: "0" }} id='cardsRow'>
-            {courses.map((course, idx) => (
-              <Col key={idx} className='cards'>
-                <Link to={`/courses/${course.path}`} className='cards-link'>
-                  <Card style={{ minHeight: "252px" }}>
-                    <Card.Body>
-                      <Card.Title className='card-title'>{course.name}</Card.Title>
-                      <div className='progress-container'>
-                        <h5 className='card-text'>Progress</h5>
-                        <div className="progress-bar">
-                          <div className="progress" style={{ width: `${course.progress * 100}%` }}></div>
-                        </div>
-                      </div>
-                      <div className='arrow-container'>
-                        <div className='arrow-tail'></div>
-                        <FaChevronRight className='arrow-head' />
-                      </div>
-                    </Card.Body>
-                  </Card>
-                </Link>
-              </Col>
-            ))}
-          </Row>
-        </Container>
+        <>
+          {student && (
+            <Container className='cards-container' style={{ padding: "0px" }} fluid>
+              <Row xs={1} md={2} className="g-4" style={{ padding: "0" }} id='cardsRow'>
+                {courses.map((course, idx) => (
+                  <Col key={idx} className='cards'>
+                    <Link to={`/courses/${course.path}`} className='cards-link'>
+                      <Card style={{ minHeight: "252px" }}>
+                        <Card.Body>
+                          <Card.Title className='card-title'>{course.name}</Card.Title>
+                          <div className='progress-container'>
+                            <h5 className='card-text'>Progress</h5>
+                            <div className="progress-bar">
+                              <div className="progress" style={{ width: `${course.progress * 100}%` }}></div>
+                            </div>
+                          </div>
+                          <div className='arrow-container'>
+                            <div className='arrow-tail'></div>
+                            <FaChevronRight className='arrow-head' />
+                          </div>
+                        </Card.Body>
+                      </Card>
+                    </Link>
+                  </Col>
+                ))}
+              </Row>
+            </Container>
+          )}
+          {teacher && (
+            <>
+            <Container className='cards-container' style={{ padding: "0px" }} fluid>
+              <Row xs={1} md={2} className="g-4" style={{ padding: "0" }} id='cardsRow'>
+                {courses.map((course, idx) => (
+                  <Col key={idx} className='cards'>
+                    <Link to={`/courses/${course.path}`} className='cards-link'>
+                      <Card style={{ minHeight: "252px" }}>
+                        <Card.Body>
+                          <Card.Title className='card-title'>{course.name}</Card.Title>
+                          <div className='progress-container'>
+                            <h5 className='card-text'>Progress</h5>
+                            <div className="progress-bar">
+                              <div className="progress" style={{ width: `${course.progress * 100}%` }}></div>
+                            </div>
+                          </div>
+                          <div className='arrow-container'>
+                            <div className='arrow-tail'></div>
+                            <FaChevronRight className='arrow-head' />
+                          </div>
+                        </Card.Body>
+                      </Card>
+                    </Link>
+                  </Col>
+                ))}
+              </Row>
+            </Container>
+            </>
+          )}
+        </>
       )}
     </>
   );
